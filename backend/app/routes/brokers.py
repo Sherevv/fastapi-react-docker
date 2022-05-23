c470f99943efb201ce80f678644c612d8e76a420
sherevv <sherevv@gmail.com>
2022-05-23 21:49:05 +0300

    Add routes for broker, refactor for using CRUD class

--- backend/app/routes/brokers.py
+++ backend/app/routes/brokers.py
@@ -1,27 +1,52 @@
 from typing import List
+from uuid import UUID
 
+from app.crud.repo import CRUDRepository, get_repository
+from app.models import Broker, BrokerCreate, BrokerRead, BrokerUpdate
 from fastapi import APIRouter, Depends
-from sqlmodel import select
-from sqlmodel.ext.asyncio.session import AsyncSession
-
-from app.models import Broker
-from app.db import get_session
 
 router = APIRouter()
 
 
-@router.get("/", response_model=List[Broker])
-async def get_brokers(session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Broker))
-    items = result.scalars().unique().all()
+@router.get("/", response_model=List[BrokerRead])
+async def get_brokers(
+    crud: CRUDRepository = Depends(get_repository(Broker)),
+):
+    items = await crud.get_list()
     return items
 
 
-@router.post("/", response_model=Broker)
-async def create_broker(*,
-                        session: AsyncSession = Depends(get_session),
-                        item: Broker):
-    session.add(item)
-    await session.commit()
-    await session.refresh(item)
+@router.get("/{item_id}", response_model=BrokerRead)
+async def get_broker(
+    item_id: UUID,
+    crud: CRUDRepository = Depends(get_repository(Broker)),
+):
+    item = await crud.get(item_id)
+    return item
+
+
+@router.post("/", response_model=BrokerRead)
+async def create_broker(
+        item: BrokerCreate,
+        crud: CRUDRepository = Depends(get_repository(Broker))
+):
+    item = await crud.create(item.dict(exclude_unset=True))
+    return item
+
+
+@router.put("/", response_model=BrokerRead)
+async def update_broker(
+        item: BrokerUpdate,
+        crud: CRUDRepository = Depends(get_repository(Broker))
+):
+    item = await crud.update(item.dict(exclude_unset=True))
+    return item
+
+
+@router.delete("/{item_id}", response_model=BrokerRead)
+async def delete_broker(
+            item_id: UUID,
+        crud: CRUDRepository = Depends(get_repository(Broker))
+):
+    item = await crud.remove(item_id)
     return item
